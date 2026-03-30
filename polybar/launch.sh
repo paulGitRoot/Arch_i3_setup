5853369f8e0cc24de7cfe74f4b5537a6dc320744
paulGitRoot <pawlos.addisu-ug@aau.edu.et>
2026-03-30 22:06:16 +0300

    wallpapers

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,6 +5,6 @@
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
